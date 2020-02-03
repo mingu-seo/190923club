8a5773a14dbd226700d020ee2d109177ebff7f9d
Seyoung <Seyoung@192.168.35.183>
2020-02-03 21:28:25 +0800

    sy

---
+++ club/src/main/java/like/LikeVO.java
@@ -0,0 +1,34 @@
+package like;
+
+public class LikeVO {
+
+	private int like_no;
+	private int post_id;
+	private int board_id;
+	private int member_id;
+	
+	public int getLike_no() {
+		return like_no;
+	}
+	public void setLike_no(int like_no) {
+		this.like_no = like_no;
+	}
+	public int getPost_id() {
+		return post_id;
+	}
+	public void setPost_id(int post_id) {
+		this.post_id = post_id;
+	}
+	public int getBoard_id() {
+		return board_id;
+	}
+	public void setBoard_id(int board_id) {
+		this.board_id = board_id;
+	}
+	public int getMember_id() {
+		return member_id;
+	}
+	public void setMember_id(int member_id) {
+		this.member_id = member_id;
+	}
+}

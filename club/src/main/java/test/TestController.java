3a2d2105ec42d90a1561e92e85b3adc23ea0934f
Seyoung <Seyoung@DESKTOP-P0BDP4I>
2020-02-24 15:12:09 +0800

    사진업로드

--- club/src/main/java/test/TestController.java
+++ club/src/main/java/test/TestController.java
@@ -14,7 +14,7 @@ import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.multipart.MultipartFile;
 
-
+  
 @Controller
 public class TestController {
 	private static final String UPLOAD_PATH = "/upload/test/";
